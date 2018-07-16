116c0a6d2c19accbcb1e5b922d99c74dfd60c86d
Giovanni Mascellani <gio@debian.org>
2018-07-16 09:13:53 +0200

    Add licensing details.

--- boot/bootloader.asm
+++ boot/bootloader.asm
@@ -1,3 +1,24 @@
+;; This file is part of asmc, a bootstrapping OS with minimal seed
+;; Copyright (C) 2018 Giovanni Mascellani <gio@debian.org>
+;; https://gitlab.com/giomasce/asmc
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+  ;; This file includes snippets taken from http://wiki.osdev.org; by
+  ;; https://wiki.osdev.org/OSDev_Wiki:Copyrights they are to be
+  ;; considered in the public domain, or that the CC0 license applies, so
+  ;; their inclusion in a GPL-3+ should be ok.
 
   bits 16
   org 0x7c00
@@ -266,6 +287,8 @@ mov eax, cr0
 or al, 1
 mov cr0, eax
 jmp 0x8:enter_protected
+
+  ;; The following snippet is taken from https://wiki.osdev.org/A20
 
 ; Function: check_a20
 ;
