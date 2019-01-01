9d9a0e1acae302c702d05194788d76cb39845446
Giovanni Mascellani <gio@debian.org>
2019-01-01 12:41:49 +0100

    Some further step towards compiling tcc.

--- diskfs/stdlib/asmc.h
+++ diskfs/stdlib/asmc.h
@@ -5,14 +5,19 @@
 // compiler
 
 // const is just a correctness checker, which can just be removed; we
-// assume that the program is correct instead of verifying it
+// assume that the program is correct instead of verifying it. Inline
+// is useless, because the compiler does not have a linking phase
 #define const
+#define inline
 
 // Same for static assertions: we assume they are correct; since the
 // compiler does not support empty statements, we replace the static
 // assertion with "int", making the statement into an empty
 // declaration
 #define _Static_assert(cond, msg) int
+
+// Ignore GCC __attribute__ constructs
+#define __attribute__(x)
 
 #define NULL 0
 
