601f936b30b76b174a5e89426fbcbe9cec7726df
Giovanni Mascellani <gio@debian.org>
2019-01-09 22:45:08 +0100

    Enable recursive tinycc compilation.

--- diskfs/run_tcc.c
+++ diskfs/run_tcc.c
@@ -9,24 +9,124 @@
 #define TCC_TARGET_X86_64 1
 //#define CONFIG_TRIPLET "x86_64-linux-gnu"
 
+#define ASMC_PREFIX "./diskfs"
+
+int __get_handles() {
+    return 0;
+}
+
 #else
 
 #define TCC_TARGET_I386 1
 // Need this, otherwise tcc does not believe it can run natively
 #define __i386__ 1
 
+#define ASMC_PREFIX "/disk1"
+
+#endif
+
+#ifndef LEVEL
+#define LEVEL 0
 #endif
 
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "libtcc.h"
+
 void receive_error(void *ctx, const char *msg) {
     printf("Received error: '%s'\n", msg);
 }
 
-#include "libtcc.h"
+int recursively_compile() {
+#if LEVEL == 10
+    printf("Let's not get mad...\n");
+    return 0;
+#endif
 
-int main(int argc, char *argv[]) {
+    int res;
+    TCCState *state = tcc_new();
+    tcc_set_options(state, "-nostdlib -nostdinc");
+    tcc_set_error_func(state, NULL, receive_error);
+    tcc_set_output_type(state, TCC_OUTPUT_MEMORY);
+
+    printf("I will now try to compile and execute tinycc recursively! (level %d)\n", LEVEL);
+
+    char buf[1024];
+    sprintf(buf, "%d", __get_handles());
+    tcc_define_symbol(state, "__HANDLES", buf);
+    sprintf(buf, "%d", LEVEL+1);
+    tcc_define_symbol(state, "LEVEL", buf);
+
+    res = tcc_add_include_path(state, ASMC_PREFIX);
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/stdlib");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/tinycc");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/tinycc-aux");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/tinycc/softfloat");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/tinycc/softfloat/include");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/tinycc/softfloat/8086");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+
+    res = tcc_add_file(state, ASMC_PREFIX "/run_tcc.c");
+    if (res) {
+        printf("tcc_add_file() failed\n");
+        return 1;
+    }
+
+    int size = tcc_relocate(state, NULL);
+    if (size == -1) {
+        printf("first tcc_relocate() failed\n");
+        return 1;
+    }
+    void *program = malloc(size);
+    res = tcc_relocate(state, program);
+    if (res == -1) {
+        printf("second tcc_relocate() failed\n");
+        return 1;
+    }
+
+    int (*start)() = tcc_get_symbol(state, "_start");
+    if (!start) {
+        printf("tcc_get_symbol() failed\n");
+        return 1;
+    }
+    printf("Recurring into the next tinycc...\n");
+    int ret = start();
+    printf("The program returned %d!\n", ret);
+    free(program);
+
+    tcc_delete(state);
+    return 0;
+}
+
+int test_gauss() {
     int res;
     TCCState *state = tcc_new();
     tcc_set_options(state, "-nostdlib -nostdinc");
@@ -35,7 +135,23 @@ int main(int argc, char *argv[]) {
 
     printf("I will now compile and execute a small test program with tinycc!\n");
 
-    res = tcc_compile_string(state, "int gauss_sum(int max) { int sum = 0; for (int i = 0; i <= max; i++) sum += i; return sum; }");
+    char buf[1024];
+    sprintf(buf, "%d", __get_handles());
+    tcc_define_symbol(state, "__HANDLES", buf);
+
+    res = tcc_add_include_path(state, ASMC_PREFIX);
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+    res = tcc_add_include_path(state, ASMC_PREFIX "/stdlib");
+    if (res) {
+        printf("tcc_add_include_path() failed\n");
+        return 1;
+    }
+
+    res = tcc_compile_string(state, "#include <stdio.h>\nint main(){} int gauss_sum(int max) { printf(\"Hello from inside!\\n\"); int sum = 0; for (int i = 0; i <= max; i++) sum += i; return sum; }");
+    //res = tcc_add_file(state, "diskfs/test.c");
     if (res) {
         printf("tcc_compile_string() failed\n");
         return 1;
@@ -55,17 +171,33 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    void (*init_stdlib)() = tcc_get_symbol(state, "__init_stdlib");
+    if (init_stdlib) {
+        printf("Initializing the standard library... ");
+        init_stdlib();
+        printf(" done!\n");
+    }
+
     int (*gauss_sum)(int) = tcc_get_symbol(state, "gauss_sum");
     if (!gauss_sum) {
         printf("tcc_get_symbol() failed\n");
         return 1;
     }
+    printf("Running the program...\n");
     int ret = gauss_sum(100);
     printf("The program returned %d!\n", ret);
     free(program);
 
     tcc_delete(state);
     return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (test_gauss()) {
+        printf("Gauss test failed...\n");
+        return 1;
+    }
+    return recursively_compile();
 }
 
 #define USE_SOFTFLOAT 1
